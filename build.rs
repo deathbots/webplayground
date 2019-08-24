5099ca8ae32594273d904912929ad4ec5df939a0
deathbots <>
2019-08-23 20:03:06 -0700

    Run command in dir in build script
    The function run_command_in_dir will now execute properly.

--- build.rs
+++ build.rs
@@ -3,18 +3,18 @@ use std::io::Result;
 use std::path::Path;
 use std::process::{Command, ExitStatus};
 
-fn run_cmd_in_dir<'a, I>(working_dir: &Path, prog_name: &str, args: I) -> Result<ExitStatus>
-where
-    I: Iterator<Item = &'a str>,
+fn run_cmd_in_dir(working_dir: &Path, prog_name: &str, args: &[&str]) -> Result<ExitStatus>
 {
-    Command::new("npm")
-        .args(args)
-        .current_dir(working_dir)
-        .status()
+  Command::new(prog_name)
+    .args(args)
+    .current_dir(working_dir)
+    .status()
 }
 
 fn main() {
     let project_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let a = &["install"];
-    run_cmd_in_dir(&Path::new(&project_dir), "npm", &["install"]).unwrap();
+    match run_cmd_in_dir(&Path::new(&project_dir), "npm", &["install"]) {
+      Ok(_) => println!("Done"),
+      Err(err) => println!("received error: {}", err)
+    }
 }
